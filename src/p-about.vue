886fbfee334e5e768d8c3c338bc9bc2c533ff767
benjaminwaterlot <32556322+benjaminwaterlot@users.noreply.github.com>
2018-01-20 08:56:50 +0100

    Adding "Donate" button

--- src/p-about.vue
+++ src/p-about.vue
@@ -7,7 +7,7 @@
 		data-href="https://www.facebook.com/SkinsOfTheRift/"
 		data-tabs="timeline"
 		data-width="500"
-		data-height="1000"
+		data-height="700"
 		data-small-header="false"
 		data-adapt-container-width="true"
 		data-hide-cover="false"
@@ -33,10 +33,17 @@
 				label="Follow SotR on Facebook"
 				type="discord">
 				</vButton>
-				<!-- <vButton
-				class="wanna-help__button"
-				type="paypal">
-				</vButton> -->
+				<div class="donate">
+					<p class="donate__title donate__title--bold">Donate to support early days of Skins of the Rift!</p>
+					<form class="donate__button" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+						<input type="hidden" name="cmd" value="_s-xclick">
+						<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHoAYJKoZIhvcNAQcEoIIHkTCCB40CAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYB7NX3OG5kzx24qb+3JWybwesl/ENcshSUHfQAZ+RVvu4NqN1+1OuGPJSmE2UJVvKo44AuCE9+38fPXKiBTw5jG393DeCZAoeKrsyIRIxQeHntp7/kZM+z+I8Y3iArRvI6frKVelJIM0VmdSlLjgMMB7WFDmSQQM+Q1MzwmQ42qUDELMAkGBSsOAwIaBQAwggEcBgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECBu/5CDYoiocgIH4hOmYE3JaH+tb3mP8+ozbz3KTkQVzhQ9rGRKPrpqwNhzzrhgz0bw2obcXRmjmko1MB1nguEGiAZzlFQbchHCX+5vea2R76kW7w0CRnLrwwX1H4hx3DbEFAFcHIe6CLHm5CkLcruVZFDDK3Qs4pmm1HtzQk0+jcBexKQljhbtp25THFByyV/lzkkREdQYhvbe1XNiJeVo261TOLVb+VxYU8W4/5Wvz5g1x1Wn8LeCcF1EtBUyGihnB9oCgwbZILbmgivpwIc5PpiuiuN1WPum0VeGQlVRmEHKLG/TsGlStc9LyJUWWaNuXdU4hvYU9DNn5mtmSWgGGAQqgggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xODAxMjAwNzMwMzFaMCMGCSqGSIb3DQEJBDEWBBQhGXvsMZJq6M+9yDXkcVYm01dfCjANBgkqhkiG9w0BAQEFAASBgB5UgUnWUXKHY1Rqaz3tk5HnutObDDx3DEVvnVkQOFYSFocOF5Vr2CYEIB0t58KeUlQzwr7yHLUh8BLpAIC4uOACcvgl6+c6aZAL2hhnB8ql7AH3j86AhevBt9zH5DWF4yBvaNBO3nEPcQSp7gM45dmOQIML5SzAm7Y+t8jpF/OI-----END PKCS7-----
+						">
+						<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+						<img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+					</form>
+					<p class="donate__title">And get your name on <strong>our patrons board</strong></p>
+			</div>
 			</div>
 
 		</div>
@@ -99,6 +106,34 @@ export default {
 
 	.wanna-help__buttons {
 		display: flex;
+	}
+
+	.donate {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-top: 20px;
+		background: linear-gradient(135deg, hsl(45, 98%, 70%), hsl(40, 97%, 55%));
+		border-radius: 5px;
+		padding: 10px;
+		text-shadow: 1px 1px 2px rgba(0,0,0,.3);
+	}
+
+	.donate__title {
+		text-align: center;
+	}
+
+	.donate__title--bold {
+		font-weight: bold;
+	}
+
+	.donate__button {
+		margin: 20px 0px;
+		
+	}
+
+	.donate__button * {
+		cursor: pointer;
 	}
 
 	@media (max-width: 650px) {
